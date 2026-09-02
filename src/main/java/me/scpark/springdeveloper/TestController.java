eceef360894fdc5773b3c951e13f3a377eda8655
Ramo <cryocore1225@gmail.com>
2026-09-02 19:02:36 +0900

    feat: add Spring Boot greeting endpoints

--- src/main/java/me/scpark/springdeveloper/TestController.java
+++ src/main/java/me/scpark/springdeveloper/TestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @GetMapping("/hi")
+    public String hi() {
+        return "안녕하세요!'http://localhost:8080/hi'에 대한 응답합니다";
+    }
+
     @GetMapping("/test")
     public String test() {
-        return "Hello World!";
+        return "안녕하세요! /test 요청에 대한 응답합니다.";
     }
 }
